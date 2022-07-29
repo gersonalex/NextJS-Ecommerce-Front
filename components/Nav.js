8fddfaf89c14a8acbe8747392bd576bd2f3c4564
Alex Gerson <alex.gerson@hotmail.com>
2022-07-28 22:32:29 -0400

    global state added

--- components/Nav.js
+++ components/Nav.js
@@ -3,15 +3,15 @@ import { FiShoppingBag } from "react-icons/fi";
 import { NavStyles, NavItems } from "../styles/NavStyles";
 
 export default function Nav() {
-    return (
-        <NavStyles>
-            <Link href={'/'}>Styled.</Link>
-            <NavItems>
-                <div>
-                    <FiShoppingBag />
-                    <h3>Cart</h3>
-                </div>
-            </NavItems>
-        </NavStyles>
-    )
+  return (
+    <NavStyles>
+      <Link href={"/"}>Styled.</Link>
+      <NavItems>
+        <div>
+          <FiShoppingBag />
+          <h3>Cart</h3>
+        </div>
+      </NavItems>
+    </NavStyles>
+  );
 }
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,43 +1,55 @@
-import { useQuery } from 'urql'
-import { GET_PRODUCT_QUERY } from '../../lib/query'
-import { useRouter } from 'next/router'
-import { ProductDetals, ProductInfo, Quantity, Buy } from '../../styles/ProductDetails';
-import {AiFillPlusCircle, AiFillMinusCircle} from 'react-icons/ai'
+import { useQuery } from "urql";
+import { GET_PRODUCT_QUERY } from "../../lib/query";
+import { useRouter } from "next/router";
+import {
+  ProductDetals,
+  ProductInfo,
+  Quantity,
+  Buy,
+} from "../../styles/ProductDetails";
+import { AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
+import { useStateContext } from "../../lib/context";
 
 export default function ProductDetail() {
+  //Use State
+  const { qty, increaseQty, decreaseQty } = useStateContext();
 
-    const { query } = useRouter();
+  //Fetch Slug
+  const { query } = useRouter();
 
-    //fetch grapghql data
-    const [results] = useQuery({
-        query: GET_PRODUCT_QUERY,
-        variables: {slug: query.slug }
-    })
+  //Fetch Graphql data
+  const [results] = useQuery({
+    query: GET_PRODUCT_QUERY,
+    variables: { slug: query.slug },
+  });
 
-    const {data, fetching, error} = results;
+  const { data, fetching, error } = results;
 
-      //check for the data coming in
-    if (fetching) return <p>Loading...</p>
-    if (error) return <p>Error {error.message}</p>
+  //check for the data coming in
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <p>Error {error.message}</p>;
 
-    // extract data
-    const {title, description, image} = data.products.data[0].attributes;
+  // extract data
+  const { title, description, image } = data.products.data[0].attributes;
 
-
-    return (
-        <ProductDetals>
-            <img src={image.data.attributes.formats.medium.url} alt={title} />
-            <ProductInfo>
-                <h3>{title}</h3>
-                <p>{description}</p>
-                <Quantity>
-                    <span>Quantity</span>
-                    <button><AiFillMinusCircle /></button>
-                    <p>0</p>
-                    <button><AiFillPlusCircle /></button>
-                </Quantity>
-                <Buy>Add to cart</Buy>
-            </ProductInfo>
-        </ProductDetals>
-    )
+  return (
+    <ProductDetals>
+      <img src={image.data.attributes.formats.medium.url} alt={title} />
+      <ProductInfo>
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <Quantity>
+          <span>Quantity</span>
+          <button>
+            <AiFillMinusCircle onClick={decreaseQty} />
+          </button>
+          <p>{qty}</p>
+          <button>
+            <AiFillPlusCircle onClick={increaseQty} />
+          </button>
+        </Quantity>
+        <Buy>Add to cart</Buy>
+      </ProductInfo>
+    </ProductDetals>
+  );
 }
