030d9c2f0345b7a5324b97b9b27d38a73345ce02
Alex Gerson <alex.gerson@hotmail.com>
2022-08-03 23:05:03 -0400

    Add remove items from the cart

--- components/Cart.js
+++ components/Cart.js
@@ -1,11 +1,12 @@
 import { AiFillMinusCircle, AiFillPlusCircle, AiPlusMinusCircle } from "react-icons/ai"
 import { useStateContext } from "../lib/context"
-import { CartWrapper, CartStyle, Card, CardInfo, EmptyStyle, Quantity } from "../styles/CartStyles"
+import { CartWrapper, CartStyle, Card, CardInfo, EmptyStyle } from "../styles/CartStyles"
+import { Quantity } from "../styles/ProductDetails"
 import { FaShoppingCart } from 'react-icons/fa'
 
 export default function Cart() {
 
-    const { cartItems, setShowCart } = useStateContext()
+    const { cartItems, setShowCart, onAdd, onRemove } = useStateContext()
 
     return (
         <CartWrapper onClick={() => setShowCart(false)}>
@@ -19,16 +20,16 @@ export default function Cart() {
                 { cartItems.length >= 1 && (
                     cartItems.map(item => {
                         return (
-                            <Card>
+                            <Card key={item.slug}>
                                 <img src={item.image.data.attributes.formats.thumbnail.url} alt={item.title} />
                                 <CardInfo>
                                     <h3>{item.title}</h3>
                                     <h3>{item.price}</h3>
                                     <Quantity>
                                         <span>Quantity</span>
-                                        <button><AiFillMinusCircle /></button>
+                                        <button onClick={() => onRemove(item)}><AiFillMinusCircle /></button>
                                         <p>{item.quantity}</p>
-                                        <button><AiFillPlusCircle /></button>
+                                        <button onClick={() => onAdd(item, 1)}><AiFillPlusCircle /></button>
                                     </Quantity>
                                 </CardInfo>
                             </Card>
